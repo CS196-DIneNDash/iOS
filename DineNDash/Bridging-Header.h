26a98a48a17db58a734c4faa7f6b94ad1c159f34
dongjiacheng <dongjiacheng12@gmail.com>
2016-04-30 18:16:23 -0500

    facebook login added

---
+++ DineNDash/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  DineNDash
+//
+//  Created by DONG JIACHENG on 4/30/16.
+//  Copyright © 2016 DineNDash. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
--- DineNDash/DineNDash/AppDelegate.swift
+++ DineNDash/DineNDash/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
@@ -23,7 +24,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window.rootViewController = ViewController()
             window.makeKeyAndVisible()
         }
-        return true
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+    }
+    
+    func application(application: UIApplication,
+                     openURL url: NSURL,
+                             sourceApplication: String?,
+                             annotation: AnyObject?) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(
+            application,
+            openURL: url,
+            sourceApplication: sourceApplication,
+            annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -42,6 +55,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+         FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
